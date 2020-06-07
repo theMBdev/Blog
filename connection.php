328d8e6f6d267f4ba6042b0da0bb4fa808fa66b2
Martin Brown <35014201+martingit9@users.noreply.github.com>
2020-06-07 18:39:22 +0100

    Conection update

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "blog-db");
+$mysqli = new mysqli("", "root", "", "blog-db");
 if($mysqli->connect_error) {
     exit('Error connecting to database'); //Should be a message a typical user could understand in production
 }
